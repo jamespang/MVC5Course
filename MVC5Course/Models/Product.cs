dd1c95ee1e11c69bea4dce6077a85b4e76227912
jamespang <ling.tien5846@gmail.com>
2017-03-21 23:35:55 +0800

    07 練習各種不同的 Views 與 Models 修改

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,6 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
     public partial class Product
@@ -24,10 +25,12 @@ namespace MVC5Course.Models
         public int ProductId { get; set; }
 
         [Required(ErrorMessage = "請輸入商品名稱 ({0})")]
+        [DisplayName("商品名稱")] // 示範修改 Model 的顯示欄位名稱
         public string ProductName { get; set; }
 
         [Required]
         [Range(10, 9999, ErrorMessage = "金額設定錯誤")]
+        [DisplayFormat(DataFormatString = "{0:N0}")] // 將 Price 顯示為沒有小數點（在 Model 中實作）
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
