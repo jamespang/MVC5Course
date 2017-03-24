857be12d6e0d3ea83c6304e466dc5118cb907c39
jamespang <ling.tien5846@gmail.com>
2017-03-24 14:12:49 +0800

    25 修改 ProductsController 的 Edit() 動作，改用 TryUpdateModel 來執行模型繫結

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -98,12 +98,11 @@ namespace MVC5Course.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        public ActionResult Edit(int id, FormCollection form)
         {
-            if (ModelState.IsValid)
+            var product = repoProduct.Find(id);
+            if (TryUpdateModel(product, new string[] { "ProductName", "Stock" }))
             {
-                var db = repoProduct.UnitOfWork.Context;
-                db.Entry(product).State = EntityState.Modified;
                 repoProduct.UnitOfWork.Commit();
                 return RedirectToAction("Index");
             }
